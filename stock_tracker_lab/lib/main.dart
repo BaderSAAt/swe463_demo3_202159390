de529b4dd69256185856923ecb65c650246c7608
Bader <bdr-mtr@outlook.com>
2026-09-08 10:52:22 +0300

    Implemented CompanyValues with lifecycle events

--- stock_tracker_lab/lib/main.dart
+++ stock_tracker_lab/lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
+import 'dart:math';
+
+
 
 void main() {
   runApp(const MyApp());
@@ -49,13 +53,17 @@ class MyHomePage extends StatelessWidget {
       'Revenue/Loss: +5%',
       key: ValueKey('revenueLoss'),
     ),
-    Column(
-      children: [
-        Text('Company 1: \$300'),
-        Text('Company 2: \$400'),
-        Text('Company 3: \$300'),
-      ],
-    ),
+
+    // Step 10////
+    const CompanyValues(),
+
+    // Column(
+    //   children: [
+    //     Text('Company 1: \$300'),
+    //     Text('Company 2: \$400'),
+    //     Text('Company 3: \$300'),
+    //   ],
+    // ),
     const AnalyticsButton(),
   ],
 ),
@@ -90,6 +98,49 @@ class _AnalyticsButtonState extends State<AnalyticsButton> {
         ),
         if (_showAnalytics)
           const Text('Detailed analytics go here...'),
+      ],
+    );
+  }
+}
+
+
+class CompanyValues extends StatefulWidget {
+  const CompanyValues({super.key});
+
+  @override
+  State<CompanyValues> createState() => _CompanyValuesState();
+}
+
+class _CompanyValuesState extends State<CompanyValues> {
+  final List<int> _companyValues = [300, 400, 300];
+  late Timer _timer;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _timer = Timer.periodic(const Duration(seconds: 1), (timer) {
+      setState(() {
+        _companyValues[0] += Random().nextInt(10) - 5;
+        _companyValues[1] += Random().nextInt(10) - 5;
+        _companyValues[2] += Random().nextInt(10) - 5;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _timer.cancel();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text('Company 1: \$${_companyValues[0]}'),
+        Text('Company 2: \$${_companyValues[1]}'),
+        Text('Company 3: \$${_companyValues[2]}'),
       ],
     );
   }
